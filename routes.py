6a93745ecbc572d1743f0239d366e352415cf244
kend1313 <kend1313>
2023-08-01 17:13:31 +1200

    test commit

---
+++ routes.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("homepage.html")
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
+
+@app.route ("/All_skins")
+
+def All_skins():
+  conn=sqlite3.connect("CS2.db")
+  cur=conn.cursor()
+  cur.execute('SELECT * FROM skins')
+  results=cur.fetchall()
+  conn.close
+  return render_template("allskins.html",results = results)
+
+@app.route('/skins/<int:id>')
+def skin(id):
+  conn = sqlite3.connect('CS2.db')
+  cur = conn.cursor()
+#skin
+def case(id):
+  conn= sqlite3.connect('CS2.db')
+  cur = conn.cursor()
+  
+  cur.execute('SELECT * FROM Skins WHERE id=?',(id,))
+  pizza = cur.fetchone()
+# baseSS
+  cur.execute('SELECT * FROM Base WHERE id=?',(id,))
+  base = cur.fetchone()
+# cur.execute("SELECT toppingname FROM Topping WHERE id = (SELECT tid FROM Pizzatopping WHERE pid= ?)",(id,))
+#topping = cur.fetchall()
+  return render_template('allskins.html',All_skins, base=base)
+
+if __name__ == "__main__":
+    app.run(debug = True)
--- website/main.py
+++
@@ -1,6 +0,0 @@
-from website import create_app 
-
-app = create_app
-
-if __name__ == '_main_':
-    app.run(debug=True)
