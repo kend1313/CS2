6a49ac7a68a7e7ad383faf97013b54ba0c718c45
kend1313 <kend1313>
2023-09-19 09:46:23 +1200

    flake8

--- home.py
+++ home.py
@@ -9,6 +9,7 @@ def home():
     return render_template("home.html")
 # display the home page, connect with home.html
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
@@ -32,6 +33,7 @@ def all_skins():
     return render_template("allskins.html", results=results)
 # display on allskins.html page with name results
 
+
 @app.route('/skin/<int:id>')
 def skin(id):
     conn = sqlite3.connect('CS2.db')
@@ -39,14 +41,14 @@ def skin(id):
     cur.execute('SELECT * FROM skin WHERE id=?', (id,))
     description = cur.fetchone()
 # print the single reult for skin
-    cur.execute('SELECT * FROM CaseBelong WHERE id=?',(id,))
+    cur.execute('SELECT * FROM CaseBelong WHERE id=?', (id,))
     CaseBelong = cur.fetchone()
 # print the single result for CaseBelong
-    cur.execute('SELECT * FROM ItemQuality WHERE id=?',(id,))
+    cur.execute('SELECT * FROM ItemQuality WHERE id=?', (id,))
     ItemQuality = cur.fetchone()
 # print the single result for ItemQuality
-    return render_template('skin.html', description=description,
-    CaseBelong=CaseBelong, ItemQuality=ItemQuality)
+    return render_template('skin.html', description=description, CaseBelong=CaseBelong, ItemQuality=ItemQuality)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
